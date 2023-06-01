8672c08b439d6156d3668847abd007614e853c0f
Binnur KURT <deepcloudlabs@gmail.com>
2023-06-01 11:41:24 +0300

    initial commit

--- lottery-consumer/src/main/java/com/example/lottery/LotteryConsumerApplication.java
+++ lottery-consumer/src/main/java/com/example/lottery/LotteryConsumerApplication.java
@@ -1,19 +1,17 @@
-package com.example.config;
+package com.example.lottery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
-@EnableDiscoveryClient
 @EnableEurekaClient
-public class ConfigServerApplication {
+@EnableDiscoveryClient
+public class LotteryConsumerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(LotteryConsumerApplication.class, args);
 	}
 
 }
--- lottery-spring.boot/src/main/java/com/example/lottery/controller/LotteryRestController.java
+++ lottery-spring.boot/src/main/java/com/example/lottery/controller/LotteryRestController.java
@@ -3,18 +3,18 @@ package com.example.lottery.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.annotation.RequestScope;
 
 import com.example.lottery.service.LotteryService;
 
 @RestController
-@RequestScope
+@RefreshScope
 @RequestMapping("/numbers")
 @CrossOrigin
 @Validated
